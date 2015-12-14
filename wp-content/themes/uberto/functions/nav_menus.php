90a3151b797e8a423cc998f530c41ee91de2a9b7
Gene Higgins <genotype@gknow.com>
2015-12-14 15:32:53 -0700

    only adding actual content to repo

--- wp-content/themes/uberto/functions/nav_menus.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-register_nav_menus( array(
-	'ci_main_menu' => __('Main Menu', 'ci_theme')
-));
-
-
-?>
--- wp-content/themes/uberto/sidebar.php
+++
@@ -1,5 +0,0 @@
-<div class="col-md-3 col-sm-4">
-	<div class="sidebar">
-		<?php dynamic_sidebar('blog-sidebar'); ?>
-	</div>
-</div>
